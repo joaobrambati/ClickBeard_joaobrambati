84bf896ca35b3c72f308c9fe6454a15aaf189265
joaoBrambatii <joao.brambati@litoraltextil.com.br>
2025-11-10 10:25:38 -0300

    feat: interface com os métodos de Barbeiro

---
+++ backend/src/application/interfaces/IBarbeiro.ts
@@ -0,0 +1,11 @@
+import { Barbeiro } from "../../domain/entities/Barbeiro";
+import { BarbeiroDto } from "../dtos/BarbeiroDto";
+import { Response } from "../response/Response";
+
+export interface IBarbeiro {
+  criar(dto: BarbeiroDto): Promise<Response<Barbeiro>>;
+  listar(): Promise<Response<Barbeiro[]>>;
+  buscarPorId(id: number): Promise<Response<Barbeiro>>;
+  atualizar(id: number, dto: BarbeiroDto): Promise<Response<boolean>>;
+  deletar(id: number): Promise<Response<boolean>>;
+}
