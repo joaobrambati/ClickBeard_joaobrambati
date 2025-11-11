adbea8e1f12bffee38c94a8886c6646ff083a190
joaoBrambatii <joao.brambati@litoraltextil.com.br>
2025-11-11 20:25:46 -0300

    feat: declaração de arquivo de rotas de Barbeiro Especialidade no server

--- backend/src/clickbeardapi/server.ts
+++ backend/src/clickbeardapi/server.ts
@@ -5,6 +5,7 @@ import swaggerJsdoc from "swagger-jsdoc";
 import barbeiroRoutes from "../clickbeardapi/routes/BarbeiroRoutes";
 import usuarioRoutes from "../clickbeardapi/routes/UsuarioRoutes";
 import especialidadeRoutes from "../clickbeardapi/routes/EspecialidadeRoutes";
+import especialidadeBarbeiroRoutes from "../clickbeardapi/routes/barbeiroEspecialidadeRoutes";
 
 const app = express();
 app.use(express.json());
@@ -27,5 +28,6 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 app.use("/api/barbeiros", barbeiroRoutes)
 app.use("/api/usuarios", usuarioRoutes)
 app.use("/api/especialidades", especialidadeRoutes)
+app.use("/api/especialidadesBarbeiros", especialidadeBarbeiroRoutes)
 
 app.listen(4000, () => console.log("Servidor rodando na porta 4000"));
